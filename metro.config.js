fc33c4f9dd944281cadd591657f44d4121f87c0a
selezai <selezai@gmail.com>
2025-03-16 18:33:50 +0200

    Simplify metro configuration for better compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 // Get the default Metro configuration
 const config = getDefaultConfig(__dirname);
-
-// Add support for importing SVG files
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
-
-// Add support for expo-router
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  'expo-router': require.resolve('expo-router'),
-};
 
 // Export the modified configuration
 module.exports = config;
